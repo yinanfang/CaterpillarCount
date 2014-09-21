84aa3630091e7e3e732fc032b5fdd9bf38f23afa
yinanfang <yinan_fang@hotmail.com>
2014-09-21 19:20:56 -0400

    Initial Commit

---
+++ CaterpillarCount/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CaterpillarCount
+//
+//  Created by Golden Compass on 9/21/14.
+//  Copyright (c) 2014 Golden Compass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CaterpillarCountTests/CaterpillarCountTests.m
@@ -0,0 +1,40 @@
+//
+//  CaterpillarCountTests.m
+//  CaterpillarCountTests
+//
+//  Created by Golden Compass on 9/21/14.
+//  Copyright (c) 2014 Golden Compass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CaterpillarCountTests : XCTestCase
+
+@end
+
+@implementation CaterpillarCountTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
